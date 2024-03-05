bd18a87a8bfef71f30be05ffc3e61e4067a30ce0
SoniaLidara <110144785+sonialidara@users.noreply.github.com>
2024-03-05 17:04:56 +0700

    Update dashboard.py

--- streamlit/dashboard.py
+++ streamlit/dashboard.py
@@ -12,10 +12,8 @@ import plotly.graph_objects as go
 
 
 # LOAD DATA
-<<<<<<< HEAD
 
 =======
->>>>>>> bfe3fdcc30fc693270f31d088ed43ec310f0bdd9
 @st.cache_resource
 def load_data():
     data = pd.read_csv("./dataset/hour.csv")
@@ -60,10 +58,8 @@ st.sidebar.markdown('3: Salju Ringan, Hujan Ringan + Badai Petir + Awan Tersebar
 st.sidebar.markdown('4: Hujan Lebat + Palet Es + Badai Petir + Kabut, Salju + Kabut')
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> bfe3fdcc30fc693270f31d088ed43ec310f0bdd9
+=======
 # VISUALIZATION
 
 # create a layout with two columns
@@ -79,11 +75,10 @@ with col1:
 
     season_count = data.groupby("season_label")["cnt"].sum().reset_index()
     fig_season_count = px.bar(season_count, x="season_label",
-<<<<<<< HEAD
+
                               y="total_count", title="Jumlah Penyewa berdasarkan Musim")
 =======
                               y="cnt", title="Season-wise Bike Share Count")
->>>>>>> bfe3fdcc30fc693270f31d088ed43ec310f0bdd9
     st.plotly_chart(fig_season_count, use_container_width=True,
                     height=400, width=600)
 
@@ -91,7 +86,7 @@ with col2:
     # Weather situation-wise bike share count
     # st.subheader("Weather Situation-wise Bike Share Count")
 
-<<<<<<< HEAD
+
     weather_count = data.groupby("weather_condition")["total_count"].sum().reset_index()
     fig_weather_count = px.bar(weather_count, x="weather_condition",
                                y="total_count", title="Jumlah Penyewa berdasar Cuaca")
@@ -99,7 +94,7 @@ with col2:
     weather_count = data.groupby("weathersit")["cnt"].sum().reset_index()
     fig_weather_count = px.bar(weather_count, x="weathersit",
                                y="cnt", title="Weather Situation-wise Bike Share Count")
->>>>>>> bfe3fdcc30fc693270f31d088ed43ec310f0bdd9
+
     # Mengatur tinggi dan lebar gambar
     st.plotly_chart(fig_weather_count, use_container_width=True,height=400, width=800)
 
